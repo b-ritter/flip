4f85f318f211040e463798c84ccc5a297f322477
b-ritter <ben@benjaminritter.com>
2015-06-01 12:12:29 -0400

    checks H or T with binary
    Converts the response from PHP into a binary string, which is then used
    to check if the flip is heads or tails. To do next: store the string in
    session storage and pull values from it rather than making a new
    request each click.

--- _js/flip.js
+++ _js/flip.js
@@ -2,7 +2,7 @@ var btn = $('a#play_flip_btn');
 var flipper = $('#hand');
 var result_display = $('#result');
 var times_flipper_played = 1;
-var response;
+var response = "";
 sessionStorage.setItem("flips","");
 
 
@@ -16,31 +16,19 @@ flipper.on('animationend',reset);
 
 var get_hot_bits = function(){
 	$.get('get_hot_bits.php',function(data){
-			var my_flip = $.trim(data["random-data"]);
-			response = "Hex: "+my_flip;
-			flip = parseInt(my_flip,16);
-			response += (" Int: "+my_flip+" Heads or Tails");
-
-			if(my_flip>=128){
-				response += ": heads";
-			}else{
-				response += ": tails";
-			}
-
-		},"json").done(function(data){
-			if(data["random-data"] !== undefined){
-				result_display.fadeIn().html(response);
+		var my_flips = $.trim(data["random-data"]);
+		my_flips = parseInt(my_flips,16).toString(2);
+		current_flip = Number(my_flips.slice(-1));
+	},"json").done(function(data){
+			if(data["random-data"] !== undefined){				
+				result_display.fadeIn().html("Hot Bits: " +current_flip);
 			}else{
 				
-				if(Math.random()>=.500000){
-					response = "Heads";
-					sessionStorage.flips+="1";
-					
+				if(Math.random()>.500000){
+					response = "js Heads";
 				}else{
-					response = "Tails";
-					sessionStorage.flips+="0";
+					response = "js Tails";
 				}
-				//console.log(sessionStorage);
 				result_display.fadeIn().html(response);
 			}
 		});
--- get_hot_bits.php
+++ get_hot_bits.php
@@ -1,7 +1,7 @@
 <?php
 	
 	$xml = simplexml_load_file(
-		"https://www.fourmilab.ch/cgi-bin/Hotbits?nbytes=1&fmt=xml&npass=1&lpass=8&pwtype=3"
+		"https://www.fourmilab.ch/cgi-bin/Hotbits?nbytes=8&fmt=xml&npass=1&lpass=8&pwtype=3"
 	);
 	
 	$json_string = json_encode($xml);
